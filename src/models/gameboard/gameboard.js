14a39ef2d99d261159a1e516335d7e2b17eb60d1
Giulio Riondino <riondino.giulio@gmail.com>
2025-08-17 15:19:29 -0700

    docs(ship,gameboard): add JSDoc-style documentation for Ship and Gameboard classes

--- src/models/gameboard/gameboard.js
+++ src/models/gameboard/gameboard.js
@@ -1,4 +1,11 @@
+/**
+ * Represents a Battleship game board.
+ */
 class Gameboard {
+  /**
+   * Creates a new game board.
+   * @param {number} size - The size of the board (default 10x10).
+   */
   constructor(size = 10) {
     this.size = size;
     this.board = Array.from({ length: size }, () => Array(size).fill(null));
@@ -6,6 +13,14 @@ class Gameboard {
     this.attacked = new Set();
   }
 
+  /**
+   * Places a ship on the board.
+   * @param {Ship} ship - The ship to place.
+   * @param {number} x - Starting x-coordinate.
+   * @param {number} y - Starting y-coordinate.
+   * @param {boolean} [isHorizontal=true] - Whether the ship is placed horizontally.
+   * @throws Will throw an error if placement is out of bounds or overlaps another ship.
+   */
   placeShip(ship, x, y, isHorizontal = true) {
     for (let i = 0; i < ship.length; i++) {
       const posX = isHorizontal ? x : x + i;
@@ -26,6 +41,13 @@ class Gameboard {
     this.ships.push(ship);
   }
 
+  /**
+   * Registers an attack on a cell.
+   * @param {number} x - X-coordinate of the attack.
+   * @param {number} y - Y-coordinate of the attack.
+   * @returns {Ship|string} The ship hit or "miss" if no ship is present.
+   * @throws Will throw an error if coordinates are out of bounds or cell was already attacked.
+   */
   receiveAttack(x, y) {
     if (x < 0 || x >= this.size || y < 0 || y >= this.size) {
       throw new Error("Cannot attack a cell out of bounds");
@@ -46,10 +68,21 @@ class Gameboard {
     return this.board[x][y];
   }
 
+  /**
+   * Checks if all ships on the board have sunk.
+   * @returns {boolean} True if every ship's isSunk() returns true.
+   */
   areAllShipsSunk() {
     return this.ships.every((ship) => ship.isSunk() === true);
   }
 
+  /**
+   * Generates a unique key for a coordinate (used internally).
+   * @private
+   * @param {number} x
+   * @param {number} y
+   * @returns {string} Coordinate key as "x,y".
+   */
   #coordKey(x, y) {
     return `${x},${y}`;
   }
--- src/models/ship/ship.js
+++ src/models/ship/ship.js
@@ -1,6 +1,15 @@
+/**
+ * Represents a ship in the game.
+ */
 class Ship {
   #size;
   #hits = 0;
+
+  /**
+   * Creates a new Ship instance.
+   * @param {number} size - The size (length) of the ship. Must be greater than 0.
+   * @throws Will throw an error if size <= 0.
+   */
   constructor(size = 2) {
     if (size <= 0) {
       throw new Error("Invalid ship size");
@@ -9,14 +18,24 @@ class Ship {
     this.#size = size;
   }
 
+  /**
+   * @returns {number} The size of the ship.
+   */
   get size() {
     return this.#size;
   }
 
+  /**
+   * @returns {number} The number of hits the ship has taken.
+   */
   get hits() {
     return this.#hits;
   }
 
+  /**
+   * Registers a hit on the ship.
+   * @throws Will throw an error if the ship is already sunk.
+   */
   onHit() {
     if (this.isSunk()) {
       throw new Error("Cannot hit any more after ship has sunk");
@@ -25,6 +44,10 @@ class Ship {
     this.#hits += 1;
   }
 
+  /**
+   * Checks if the ship has sunk.
+   * @returns {boolean} True if hits equal size, false otherwise.
+   */
   isSunk() {
     return this.#hits === this.#size ? true : false;
   }
